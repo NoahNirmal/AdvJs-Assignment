95cfdc5cdd9a20b01ebf23065ed8cc3b1a7de9d5
Noah-1711 <noahnirmal11@gmail.com>
2023-01-06 11:17:48 +0530

    Adv-JS

--- AdvancedJs-403-02/index.js
+++ AdvancedJs-403-02/index.js
@@ -1,52 +1,39 @@
-var button = document.querySelectorAll(".drum")
- var nobtn=button.length
-console.log(nobtn)
-
-for(let i=0;i<nobtn;i++){
+var button = document.getElementsByClassName("drum")
+for(let i=0;i<button.length;i++){
     button[i].addEventListener("click",function(event){
-        console.log("clicked")
         var buttontext = button[i].innerHTML;
-        console.log(buttontext)
         drumsound(buttontext)
-
     })
     
 }
 
 function drumsound(key){
     if(key=='w'){
-        console.log("hello")
         var wsound= new Audio("sounds/w.mp3")
         wsound.play()
     }
     else  if(key=='a'){
-        console.log("hello")
         var asound= new Audio("sounds/a.mp3")
         asound.play()
     }
     else  if(key=='s'){
-        console.log("hello")
         var ssound= new Audio("sounds/s.mp3")
         ssound.play()
     }
     else  if(key=='d'){
-        console.log("hello")
         var dsound= new Audio("sounds/d.mp3")
         dsound.play()
     }
     else  if(key=='j'){
-        console.log("hello")
         var jsound= new Audio("sounds/j.mp3")
         jsound.play()
     }
     else  if(key=='k'){
-        console.log("hello")
         var ksound= new Audio("sounds/k.mp3")
         ksound.play()
     }
 
     else  if(key=='l'){
-        console.log("hello")
         var lsound= new Audio("sounds/l.mp3")
         lsound.play()
     }
