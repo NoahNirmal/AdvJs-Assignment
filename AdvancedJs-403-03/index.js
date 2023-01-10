fbb9a4c0f456772fd9fa6c0921868eeaa6746c6e
Noah-1711 <noahnirmal11@gmail.com>
2023-01-10 11:12:25 +0530

    Adv-JS 403-03

--- AdvancedJs-403-03/index.js
+++ AdvancedJs-403-03/index.js
@@ -8,11 +8,14 @@ function calculate(){
     var people=+document.getElementById("people").value
     if(amount==""|| people==""||service==0){
         alert("Please Enter Info")
+    } 
+    else{
+
+        var total = (amount * service) / people;
+        var result= document.getElementById("resulth1")
+        result.innerHTML=` <p>Tip Amount</p><h1> <sup>$</sup>${total.toFixed(2)}</h1><p>each<p>`
     }
 
-    var total = (amount * service) / people;
-    var result= document.getElementById("resulth1")
-    result.innerHTML=` <p>Tip Amount</p><h1> <sup>$</sup>${total.toFixed(2)}</h1><p>each<p>`
 
 
 }
